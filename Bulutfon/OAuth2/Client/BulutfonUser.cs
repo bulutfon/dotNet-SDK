2d19408fd3b76fbb67eec9f676cd057335a9b3eb
ahmetUrel <yazemo@gmail.com>
2015-05-25 13:30:24 +0300

    #1 Kullanıcı bilgileri eklendi

---
+++ Bulutfon/OAuth2/Client/BulutfonUser.cs
@@ -0,0 +1,20 @@
+﻿using OAuth2.Models;
+
+namespace Bulutfon.OAuth2.Client
+{
+    /// <summary>
+    /// Bulutfon kullanıcı bilgileri
+    /// </summary>
+    public class BulutfonUser : UserInfo
+    {
+        /// <summary>
+        /// Ülkesi
+        /// </summary>
+        public string Country { get; set; }
+
+        /// <summary>
+        /// Açıklama
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
