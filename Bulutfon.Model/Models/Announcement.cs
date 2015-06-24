94a45c2b2c93754af4a75acf2f5221be0fb58328
ahmetUrel <yazemo@gmail.com>
2015-06-24 14:48:33 +0300

    #2 Announcement

---
+++ Bulutfon.Model/Models/Announcement.cs
@@ -0,0 +1,33 @@
+﻿namespace Bulutfon.Model.Models
+{
+    /// <summary>
+    /// Ses dosyası
+    /// </summary>
+    public class Announcement
+    {
+        /// <summary>
+        /// Ses Dosyası id'si
+        /// </summary>
+        public int id { get; set; }
+
+        /// <summary>
+        /// Ses Dosyası Adı
+        /// </summary>
+        public string name { get; set; }
+
+        /// <summary>
+        /// Ses dosyası dosya adı
+        /// </summary>
+        public string file_name { get; set; }
+
+        /// <summary>
+        /// Bekleme müziği olarak seçilip seçilmediği
+        /// </summary>
+        public bool is_on_hold_music { get; set; }
+
+        /// <summary>
+        /// Oluşturulma tarihi
+        /// </summary>
+        public string created_at { get; set; }
+    }
+}
