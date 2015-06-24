04b6dae3318076563b88c5387ee392b12a0f3f78
ahmetUrel <yazemo@gmail.com>
2015-06-24 16:06:28 +0300

    #2 CDR

---
+++ Bulutfon.Model/Models/ResponseObjects/CdrsResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Bulutfon.Model.Models.ResponseObjects
+{
+    public class CdrsResponse
+    {
+        public List<Cdr> cdrs { get; set; }
+        public Pagination pagination { get; set; }
+    }
+}
