cd5e4ee72a99ccf4e663c09dbf710c78bfd6b96a
ahmetUrel <yazemo@gmail.com>
2015-06-25 12:29:46 +0300

    #2 MessageTitles

---
+++ Bulutfon.Model/Models/MessageTitle.cs
@@ -0,0 +1,26 @@
+﻿namespace Bulutfon.Model.Models
+{
+    /// <summary>
+    /// Mesaj Başlığı
+    /// </summary>
+    public class MessageTitle
+    {
+        /// <summary>
+        /// Mesaj başlığının id'si
+        /// </summary>
+        public int id { get; set; }
+
+        /// <summary>
+        /// Tanımlı Mesaj Başlığı
+        /// </summary>
+        public string name { get; set; }
+
+        /// <summary>
+        /// Onay durumu
+        /// DRAFT: Taslak (Henüz onaylı değil)
+        /// REJECTED: Reddedildi
+        /// CONFIRMED: Onaylandı
+        /// </summary>
+        public string state { get; set; }
+    }
+}
---
+++ Bulutfon.Model/Models/ResponseObjects/MessageTitlesResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Bulutfon.Model.Models.ResponseObjects
+{
+    /// <summary>
+    /// Panelden tanımlanmış mesaj başlıkları
+    /// GET /message-titles.json?access_token=xxx*
+    /// </summary>
+    public class MessageTitlesResponse
+    {
+        public List<MessageTitle> message_titles { get; set; }
+    }
+}
