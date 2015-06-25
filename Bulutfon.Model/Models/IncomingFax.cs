2c79354d4e34a2469939a745ba20bb62d67e35de
ahmetUrel <yazemo@gmail.com>
2015-06-25 12:20:18 +0300

    #2 Gelen fakslar

---
+++ Bulutfon.Model/Models/IncomingFax.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bulutfon.Model.Models
+{
+    /// <summary>
+    /// Gelen Faks
+    /// </summary>
+    public class IncomingFax
+    {
+        /// <summary>
+        /// Faksın bezersiz id'si
+        /// </summary>
+        public string uuid { get; set; }
+
+        /// <summary>
+        /// Faksı gönderen
+        /// </summary>
+        public object sender { get; set; }
+
+        /// <summary>
+        /// Faksın geldiği santral numarası
+        /// </summary>
+        public object receiver { get; set; }
+
+        /// <summary>
+        /// Faksın geldiği zaman damgası
+        /// </summary>
+        public string created_at { get; set; }
+    }
+}
---
+++ Bulutfon.Model/Models/ResponseObjects/IncomingFaxesResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Bulutfon.Model.Models.ResponseObjects
+{
+    /// <summary>
+    /// Gelen fakslar
+    /// GET /incoming-faxes.json
+    /// </summary>
+    public class IncomingFaxesResponse
+    {
+        public List<IncomingFax> incoming_faxes { get; set; }
+    }
+}
