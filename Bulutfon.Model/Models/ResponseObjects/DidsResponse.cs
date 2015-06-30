d97c6ef2a88d22c81364d36d6b31fee4d8cf8ed5
ahmetUrel <yazemo@gmail.com>
2015-06-30 16:46:14 +0300

    MVC4 desteği

--- Bulutfon.Model/Models/ResponseObjects/DidsResponse.cs
+++ Bulutfon.Model/Models/ResponseObjects/DidsResponse.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections.Generic;
 
-namespace Bulutfon.Model.Models.ResponseObjects
-{
+namespace Bulutfon.Model.Models.ResponseObjects {
+
     /// <summary>
     /// Santralde kullanılan telefon numaraları hakkında bilgiler
     /// GET /dids.json
     /// </summary>
-    public class DidsResponse
+    public class DidsResponse 
     {
+
         public List<Did> dids { get; set; }
+
+        public static List<Did> Get(string token) 
+        {
+            return null; //BulutfonClient.GetObject<DidsResponse>("https://api.bulutfon.com/dids", token).dids;
+        }
     }
 }
