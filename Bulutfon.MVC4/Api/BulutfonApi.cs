04f4524d85b9f2f73c024542dca4c8ec478c6fcc
ahmetUrel <yazemo@gmail.com>
2015-06-30 18:37:12 +0300

    API

--- Bulutfon.MVC4/Api/BulutfonApi.cs
+++ Bulutfon.MVC4/Api/BulutfonApi.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Net;
 using Bulutfon.Model.Models;
+using Bulutfon.Model.Models.ResponseObjects;
 using Newtonsoft.Json;
 
 namespace Bulutfon.MVC4.Api {
@@ -15,7 +16,7 @@ namespace Bulutfon.MVC4.Api {
                 var keyValue = string.Empty;
                 if (!string.IsNullOrEmpty(key))
                     keyValue = string.Format("/{0}", keyValue);
-                string str = client.DownloadString(uri + keyValue + tokenKey + token);
+                string str = client.DownloadString(Endpoint + uri + keyValue + tokenKey + token);
                 if (string.IsNullOrEmpty(str)) {
                     return null;
                 }
@@ -23,8 +24,163 @@ namespace Bulutfon.MVC4.Api {
             }
         }
 
+        /// <summary>
+        /// Anonslar
+        /// </summary>
+        /// <param name="token">Access token</param>
+        /// <returns>Anons listesi</returns>
         public static List<Announcement> GetAnnouncements(string token) {
-            return GetObject<AnnouncementsResponse>("https://api.bulutfon.com/dids", token).announcements;
+            return GetObject<AnnouncementsResponse>("announcement", token).announcements;
+        }
+
+        /// <summary>
+        /// Otomatik aramalar
+        /// </summary>
+        /// <param name="token">Access token</param>
+        /// <returns>Otomatik aramalar</returns>
+        public static AutomaticCall GetAutomaticCall(string token) {
+            return GetObject<AutomaticCallResponse>("automatic-calls", token).automatic_call;
+        }
+
+        /// <summary>
+        /// Cdr'lar
+        /// </summary>
+        /// <param name="token">Access token</param>
+        /// <returns>Cdr listesi</returns>
+        public static List<Cdr> GetCdrs(string token) {
+            return GetObject<CdrsResponse>("cdrs", token).cdrs;
+        }
+
+        /// <summary>
+        /// Cdr
+        /// </summary>
+        /// <param name="token">Access token</param>
+        /// <param name="id">Cdr id</param>
+        /// <returns>Cdr</returns>
+        public static Cdr GetCdr(string token, string id) {
+            return GetObject<CdrResponse>("cdrs", token, id).cdr;
+        }
+
+        /// <summary>
+        /// Did'ler
+        /// </summary>
+        /// <param name="token">Access token</param>
+        /// <returns>Did listesi</returns>
+        public static List<Did> GetDids(string token) {
+            return GetObject<DidsResponse>("dids", token).dids;
+        }
+
+        /// <summary>
+        /// Did
+        /// </summary>
+        /// <param name="token">Access token</param>
+        /// <param name="id">Id</param>
+        /// <returns>Did</returns>
+        public static Did GetDid(string token, string id) {
+            return GetObject<DidResponse>("dids", token, id).did;
+        }
+
+        /// <summary>
+        /// Extensions
+        /// </summary>
+        /// <param name="token">Access token</param>
+        /// <returns></returns>
+        public static List<Extension> GetExtensions(string token) {
+            return GetObject<ExtensionsResponse>("extensions", token).extensions;
+        }
+
+        /// <summary>
+        /// Extension
+        /// </summary>
+        /// <param name="token">Access token</param>
+        /// <param name="id">Id</param>
+        /// <returns>Extension</returns>
+        public static Extension GetExtension(string token, string id) {
+            return GetObject<ExtensionResponse>("extensions", token, id).extension;
+        }
+
+        /// <summary>
+        /// Gruplar
+        /// </summary>
+        /// <param name="token">Access token</param>
+        /// <returns>Grup listesi</returns>
+        public static List<Group> GetGroups(string token) {
+            return GetObject<GroupsResponse>("groups", token).groups;
+        }
+
+        /// <summary>
+        /// Grup
+        /// </summary>
+        /// <param name="token">Access token</param>
+        /// <param name="id">Id</param>
+        /// <returns>Grup</returns>
+        public static Group GetGroup(string token, string id) {
+            return GetObject<GroupResponse>("groups", token, id).group;
+        }
+
+        /// <summary>
+        /// Gelen fakslar
+        /// </summary>
+        /// <param name="token">Access token</param>
+        /// <returns>Gelen faks listesi</returns>
+        public static List<IncomingFax> GetIncomingFaxes(string token) {
+            return GetObject<IncomingFaxesResponse>("incoming-faxes", token).incoming_faxes;
+        }
+
+        /// <summary>
+        /// Kullanıcı bilgileri
+        /// </summary>
+        /// <param name="token">Access token</param>
+        /// <returns>Kullanıcı bilgileri</returns>
+        public static User GetUser(string token) {
+            return GetObject<MeResponse>("me", token).user;
+        }
+
+        /// <summary>
+        /// PBX
+        /// </summary>
+        /// <param name="token">Access token</param>
+        /// <returns>PBX</returns>
+        public static Pbx GetPbx(string token) {
+            return GetObject<MeResponse>("me", token).pbx;
+        }
+
+        /// <summary>
+        /// Mesajlar
+        /// </summary>
+        /// <param name="token">Access token</param>
+        /// <returns>Mesaj listesi</returns>
+        public static List<Message> GetMessages(string token) {
+            return GetObject<MessagesResponse>("messages", token).messages;
+        }
+
+        /// <summary>
+        /// Mesaj
+        /// </summary>
+        /// <param name="token">Access token</param>
+        /// <param name="id">Id</param>
+        /// <returns>Mesaj</returns>
+        public static Message GetMessage(string token, string id) {
+            return GetObject<MessageResponse>("messages", token, id).message;
+        }
+
+        /// <summary>
+        /// Gönderilen fakslar
+        /// </summary>
+        /// <param name="token">Access token</param>
+        /// <returns>Faks listesi</returns>
+        public static List<Fax> GetFaxes(string token) {
+            return GetObject<OutgoingFaxesResponse>("outgoing-faxes", token).faxes;
+        }
+
+        /// <summary>
+        /// Gönderilmiş faks
+        /// </summary>
+        /// <param name="token">Access token</param>
+        /// <param name="id">Id</param>
+        /// <returns>Faks</returns>
+        public static Fax GetFax(string token, string id) {
+            return GetObject<OutgoingFaxResponse>("outgoing-faxes", token, id).fax;
         }
     }
 }
